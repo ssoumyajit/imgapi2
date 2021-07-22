b2129f1d35d55e093a3d96272686ac25ea2cf7bb
River-r <ssoumyajit94@gmail.com>
2021-07-22 11:42:35 +0530

    going to work on dj_rest_auth again.

--- imgapiv1/settings.py
+++ imgapiv1/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',  # should be above the session middleware , django docs
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
